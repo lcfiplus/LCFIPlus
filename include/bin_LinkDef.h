e8d600f9d4e84ca04421abb9fc730cc39f66836b
tomohiko <tomohiko@2be2d611-926f-4194-8cc1-5bf080a0dd14>
2011-11-18 12:31:00 +0000

    add new file forgotten in the last update

---
+++ include/bin_LinkDef.h
@@ -0,0 +1,37 @@
+#ifndef EVENTDISPLAY_LINKDEF_H_
+#define EVENTDISPLAY_LINKDEF_H_
+
+#ifdef __CINT__
+
+#pragma link C++ class lcfiplus::Track+;
+#pragma link C++ class std::vector<const lcfiplus::Track*>+;
+#pragma link C++ class lcfiplus::Neutral+;
+#pragma link C++ class std::vector<const lcfiplus::Neutral*>+;
+#pragma link C++ class lcfiplus::MCParticle+;
+#pragma link C++ class std::vector<const lcfiplus::MCParticle*>+;
+#pragma link C++ class lcfiplus::Vertex+;
+#pragma link C++ class std::vector<const lcfiplus::Vertex*>+;
+#pragma link C++ class lcfiplus::MCVertex+;
+#pragma link C++ class std::vector<const lcfiplus::MCVertex*>+;
+#pragma link C++ class lcfiplus::Jet+;
+#pragma link C++ class std::vector<const lcfiplus::Jet*>+;
+
+#pragma link C++ class lcfiplus::LCIOStorer;
+#pragma link C++ class lcfiplus::TreeStorer;
+#pragma link C++ class lcfiplus::EventStore;
+
+#pragma link C++ class lcfiplus::LcfiplusAlgorithm;
+#pragma link C++ class lcfiplus::BuildUpVertex;
+#pragma link C++ class lcfiplus::JetClustering;
+#pragma link C++ class lcfiplus::PrimaryVertexFinder;
+#pragma link C++ class lcfiplus::FlavorTag;
+#pragma link C++ class lcfiplus::MakeNtuple;
+#pragma link C++ class lcfiplus::TrainMVA;
+#pragma link C++ class lcfiplus::ReadMVA;
+
+#pragma link C++ class lcfiplus::EventNavigator;
+#pragma link C++ function eventDisplay;
+
+#endif
+
+#endif
