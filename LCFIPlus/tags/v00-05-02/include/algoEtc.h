a2428a2eaa11164ede98ffbfa8aed1ad5c7c6437
engels <engels@2be2d611-926f-4194-8cc1-5bf080a0dd14>
2012-10-09 16:11:09 +0000

    tagged v00-05-02

---
+++ LCFIPlus/tags/v00-05-02/include/algoEtc.h
@@ -0,0 +1,26 @@
+// algoEtc.h
+
+#ifndef AlgoEtc_h
+#define AlgoEtc_h 1
+
+#include "lcfiplus.h"
+#include "JetFinder.h"
+
+namespace lcfiplus{
+namespace algoEtc{
+
+	// beam pseudo-tracks for primary vertex fitter
+	void makeBeamTracks(Track *&t1, Track *&t2);
+	void makeBeamVertex(Vertex *&vtx);
+	void connectVerticesToJets(const JetVec &jets, const vector<Vertex *> &vtcs, vector<vector<Vertex *> > &jetVertices, vector<vector<const Track *> > &jetResidualTracks, const Vertex *ip = 0);
+	vector<const Track *> extractTracks(VertexVec &vtx);
+	double calcThrust( vector<TVector3>& list, TVector3 &taxis );
+
+	bool SimpleSecMuonFinder(const Track *tr, double d0sigth, double z0sigth, double maxpos, double mudepmin,
+		double ecaldepmin, double ecaldepmax, double hcaldepmin, double hcaldepmax, double maxclusterpertrackenergy = 10.);
+	bool SimpleSecElectronFinder(const Track *tr, double d0sigth, double z0sigth, double maxpos, double emin,
+		double minfracecal, double minecalpertrackenergy, double maxecalpertrackenergy);
+
+}}
+
+#endif
