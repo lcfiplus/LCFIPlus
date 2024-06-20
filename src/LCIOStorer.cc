09677ac5415c943d63c2b717b021a1b0bbac009d
tmadlener <thomas.madlener@desy.de>
2024-06-20 14:41:03 +0200

    Fix warning about catching exception by value

--- src/LCIOStorer.cc
+++ src/LCIOStorer.cc
@@ -453,7 +453,7 @@ void LCIOStorer::SetEvent(lcio::LCEvent* evt) {
 	  //cal. corrected mass
 	  track->setCorrEnergy(pmass[PID.getParticleID(pfo,pidAlgoID).getPDG()]);
 	  //track->swapEnergy();  //really temporal need flag...
-	}catch(UTIL::UnknownAlgorithm e){
+	}catch(UTIL::UnknownAlgorithm& e){
 	}
 	
 	//tempolary
