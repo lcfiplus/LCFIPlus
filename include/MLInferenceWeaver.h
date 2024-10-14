2cf41b42bd5c643f86cb49ba3b0261c3f22ef29e
Tomohiko Tanabe <tomohikosan@gmail.com>
2024-10-14 15:49:23 +0900

    variable names are automatically read from JSON

--- include/MLInferenceWeaver.h
+++ include/MLInferenceWeaver.h
@@ -24,8 +24,43 @@ class MLInferenceWeaver : public Algorithm {
   std::string _jetCollectionName;
   std::string _jsonFileName;
   std::string _onnxFileName;
+  std::string _outputParameterName;
+  std::vector<std::string> _outputVariables;
   rv::RVec<std::string> _variables; //!
   WeaverInterface* _weaver; //!
+
+  struct PreprocessParams {
+    struct VarInfo {
+      VarInfo() {}
+      VarInfo(float imedian,
+              float inorm_factor,
+              float ireplace_inf_value,
+              float ilower_bound,
+              float iupper_bound,
+              float ipad)
+          : center(imedian),
+            norm_factor(inorm_factor),
+            replace_inf_value(ireplace_inf_value),
+            lower_bound(ilower_bound),
+            upper_bound(iupper_bound),
+            pad(ipad) {}
+
+      float center{0.};
+      float norm_factor{1.};
+      float replace_inf_value{0.};
+      float lower_bound{-5.};
+      float upper_bound{5.};
+      float pad{0.};
+    };
+    std::string name;
+    size_t min_length{0}, max_length{0};
+    std::vector<std::string> var_names;
+    std::unordered_map<std::string, VarInfo> var_info_map;
+    VarInfo info(const std::string& name) const { return var_info_map.at(name); }
+    void dumpVars() const;
+  };
+  std::unordered_map<std::string, PreprocessParams> prep_info_map_; //!
+  void parseJSON(const string& json_filename);
 
   ClassDef(MLInferenceWeaver,1);
 };
--- src/MLInferenceWeaver.cc
+++ src/MLInferenceWeaver.cc
@@ -2,26 +2,66 @@
 #include <map>
 #include <string>
 #include <sstream>
+#include <fstream>
+#include <iostream>
 
 #include "lcfiplus.h"
 #include "process.h"
 #include "MLInputGenerator.h"
 #include "MLInferenceWeaver.h"
 #include "WeaverInterface.h"
+#include "nlohmann/json.hpp"
 
 using namespace lcfiplus;
 using namespace MLInputGenerator;
 
-void MLInferenceWeaver::init(Parameters* param){
+void MLInferenceWeaver::parseJSON(const string& json_filename) {
+  std::ifstream json_file(json_filename);
+  std::vector<std::string> input_names;
+  std::vector<std::string> output_names;
+  try {
+    const auto json = nlohmann::json::parse(json_file);
+
+    // process output names
+    json.at("output_names").get_to(output_names);
+    for (const auto& output : output_names) {
+      _outputVariables.emplace_back(output);
+    }
+
+    // process input names
+    json.at("input_names").get_to(input_names);
+    for (const auto& input : input_names) {
+
+      // skip if string ends with _mask
+      const string ending = "_mask";
+      if (input.compare(input.size()-ending.size(), ending.size(), ending) == 0) {
+        continue;
+      }
+
+      const auto& group_params = json.at(input);
+      auto& info = prep_info_map_[input];
+      info.name = input;
+      // retrieve the variables names
+      group_params.at("var_names").get_to(info.var_names);
+      for (const auto& v : info.var_names) {
+        _variables.emplace_back(v);
+      }
+    }
+  } catch (const nlohmann::json::exception& exc) {
+    throw std::runtime_error("Failed to parse input JSON file '" + json_filename + "'.\n" + exc.what());
+  }
+}
+
+void MLInferenceWeaver::init(Parameters* param) {
   Algorithm::init(param);
   _jetCollectionName = param->get("MLInferenceWeaver.JetCollectionName",string("RefinedJets"));
   string privtx = param->get("PrimaryVertexCollectionName",string("PrimaryVertex"));
   Event::Instance()->setDefaultPrimaryVertex(privtx.c_str());
   _jsonFileName = param->get("MLInferenceWeaver.JsonFileName",string("preprocess.json"));
   _onnxFileName = param->get("MLInferenceWeaver.OnnxFileName",string("test.onnx"));
-  std::vector<string> varNames;
-  param->fetchArray("MLInferenceWeaver.Variables", varNames);
-  _variables = varNames; // needed to convert to RVec<string>
+  _outputParameterName = param->get("MLInferenceWeaver.OutputParameterName",string("weaver"));
+  
+  parseJSON(_jsonFileName);
   _weaver = new WeaverInterface(_onnxFileName, _jsonFileName, _variables);
   MLInputGenerator::init();
 }
@@ -86,7 +126,13 @@ void MLInferenceWeaver::process() {
           val = f(tr,privtx);
         }
 
-        /*
+        cand_vars.push_back(val);
+      }
+
+      for (auto neu: neutrals) {
+
+        float val(0.);
+
         if (std::holds_alternative<function<double(const Neutral*)> >(func)) {
           auto f = std::get<function<double(const Neutral*)> >(func);
           val = f(neu);
@@ -101,19 +147,31 @@ void MLInferenceWeaver::process() {
           auto f = std::get<function<double(const Neutral*, const Vertex*)> >(func);
           val = f(neu,privtx);
         }
-        */
-       
+
         cand_vars.push_back(val);
       }
 
       vars.push_back(cand_vars);
     }
-    out.emplace_back(_weaver->run(vars));
+
+    rv::RVec<float> res = _weaver->run(vars);
+    out.emplace_back(res);
+
+    assert(res.size() == _outputVariables.size());
+
+    Parameters param;
+    for (size_t i=0; i<res.size(); ++i) {
+      param.add( _outputVariables[i].data(), res[i] );
+    }
+    //param.add( "Category", (double)category );
+    jet->addParam( _outputParameterName.data(), param );
   }
 
+  /*
   for (unsigned int i=0; i<out.size(); ++i) {
     std::cout << "  j[" << i << "]: " << out[i] << std::endl;
   }
+  */
 }
 
 void MLInferenceWeaver::end() {
